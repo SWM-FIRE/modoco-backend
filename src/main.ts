4d4d5a9aa04f0bab048a139ee88e9f8709fc3c66
Juhyeong Ko <dury.ko@gmail.com>
2022-06-28 04:03:19 +0900

    FIRE-163 Add redis adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { RedisIoAdapter } from './adapters/redis.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // redis
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
