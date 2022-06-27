598faa7b89ab95e368c7a5b8744cb0c9a151439a
IamGroooooot <dury.ko@gmail.com>
2022-06-28 03:41:06 +0900

    🔧 Update .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
