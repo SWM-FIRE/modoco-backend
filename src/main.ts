b3fab21c31abfd56c7903da0d0a3707130850759
Juhyeong Ko <dury.ko@gmail.com>
2022-07-21 17:32:44 +0900

    Refactor main.ts bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,52 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { RedisIoAdapter } from './adapters/redis.adapter';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NewrelicInterceptor } from './interceptors/newrelic.interceptor';
-import helmet from 'helmet';
 import { join } from 'path';
+import helmet from 'helmet';
 
+const logger = new Logger('Modoco Bootstrap');
+
+/**
+ * bootstrap server
+ */
 async function bootstrap() {
+  try {
+    // create express application
+    const app = await createServer();
+
+    // pre init server
+    preInitServer(app);
+
+    // connect to redis
+    const redisIoAdapter = await connectRedis(app);
+
+    // init server
+    await initServer(app, redisIoAdapter);
+  } catch (error) {
+    logger.error(error.message);
+  }
+}
+
+/**
+ * Create nestExpressApplication application
+ * @returns {Promise<NestExpressApplication>} NestExpressApplication app
+ */
+async function createServer(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  return app;
+}
 
-  // static assets for socket.io client]
-  // to disable contentSecurityPolicy because it breaks the client it is placed above helmet
-  app.useStaticAssets(join(__dirname, '..', 'static'));
+/**
+ * use middleware that should be executed before server init
+ * @param {NestExpressApplication} app NestExpressApplication app
+ */
+function preInitServer(app: NestExpressApplication): void {
+  // should be executed before helmet to bypass helmet
+  hostStaticAssets(app);
 
-  // helmet middleware should be placed before any other middleware
+  // helmet middleware for security enhancement
   app.use(helmet());
 
   // cors
@@ -24,6 +57,7 @@ async function bootstrap() {
     'http://localhost:3000',
     'https://localhost:3000',
   ];
+
   const corsOptionsDelegate = function (req, callback) {
     let corsOptions;
     if (allowlist.indexOf(req.header('Origin')) !== -1) {
@@ -33,22 +67,45 @@ async function bootstrap() {
     }
     callback(null, corsOptions); // callback expects two parameters: error and options
   };
-  app.enableCors(corsOptionsDelegate);
 
-  // redis
+  app.enableCors(corsOptionsDelegate);
+}
+
+/**
+ * create RedisIoAdapter and connect to redis
+ * @param {NestExpressApplication} app NestExpressApplication app
+ * @returns {Promise<RedisIoAdapter>} redis adapter
+ */
+async function connectRedis(
+  app: NestExpressApplication,
+): Promise<RedisIoAdapter> {
   const redisIoAdapter = new RedisIoAdapter(app);
   await redisIoAdapter.connectToRedis();
+
+  return redisIoAdapter;
+}
+
+/**
+ * host static asset using express static middleware
+ * this should be executed before helmet to bypass helmet
+ * @param {NestExpressApplication} app NestExpressApplication app
+ */
+function hostStaticAssets(app: NestExpressApplication): void {
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+}
+
+/**
+ * initialize server
+ * @param {NestExpressApplication} app NestExpressApplication app
+ * @param {RedisIoAdapter} redisIoAdapter redis adapter
+ */
+async function initServer(
+  app: NestExpressApplication,
+  redisIoAdapter: RedisIoAdapter,
+) {
   app.useWebSocketAdapter(redisIoAdapter);
-
-  // newrelic interceptor
   app.useGlobalInterceptors(new NewrelicInterceptor());
-
-  // validation pipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT || 3000);
 }
