24772ec8710238f57ee2c70c6e5fc3855738f4a8
Juhyeong Ko <dury.ko@gmail.com>
2022-08-04 14:40:35 +0900

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getWelcomeMessage(): string {
-    return 'Welcome to Modoco Server';
+    return 'Welcome to Modoco API Server';
   }
 }
