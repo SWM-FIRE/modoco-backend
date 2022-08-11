b1394c7d4e9abe7a6037297d07f3a76778c34705
Juhyeong Ko <dury.ko@gmail.com>
2022-08-11 12:16:37 +0900

    Upgrade newrelic logging level and Enable browser monitoring
    Upgraded newrelic logging level to trace, Enabled browser monitoring option

--- src/interceptors/newrelic.interceptor.ts
+++ src/interceptors/newrelic.interceptor.ts
@@ -5,6 +5,30 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const newrelic = require('newrelic');
+
+@Injectable()
+export class NewrelicInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return newrelic.startWebTransaction(context.getHandler().name, function () {
+      const transaction = newrelic.getTransaction();
+      return next.handle().pipe(() => transaction.end());
+    });
+  }
+}
+
+/**
+ * Log New Relic Agent 
+ *
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const util = require('util');
@@ -14,22 +38,23 @@ const newrelic = require('newrelic');
 @Injectable()
 export class NewrelicInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // console.log(
-    //   `Child Interceptor before: ${util.inspect(context.getHandler().name)}`,
-    // );
+    console.log(
+      `Child Interceptor before: ${util.inspect(context.getHandler().name)}`,
+    );
     return newrelic.startWebTransaction(context.getHandler().name, function () {
       const transaction = newrelic.getTransaction();
-      // const now = Date.now();
+      const now = Date.now();
       return next.handle().pipe(
         tap(() => {
-          // console.log(
-          //   `Child Interceptor after: ${util.inspect(
-          //     context.getHandler().name,
-          //   )}`,
-          // );
+          console.log(
+            `Child Interceptor after: ${util.inspect(
+              context.getHandler().name,
+            )}`,
+          );
           return transaction.end();
         }),
       );
     });
   }
 }
+*/
