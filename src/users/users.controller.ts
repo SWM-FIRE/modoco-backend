2dec9a274d0051184dc58904789ecc243faab0fa
Juhyeong Ko <dury.ko@gmail.com>
2022-09-05 19:17:58 +0900

    Fix user controller
    Fix controller to parse uid as number

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateUserDTO, UpdateUserDTO } from './dto';
 import { UsersService } from './users.service';
 import { JwtGuard } from '../auth/guard';
@@ -29,33 +29,38 @@ export class UsersController {
     return this.usersService.createUser(user);
   }
 
+  @ApiBearerAuth('access_token')
   @UseGuards(JwtGuard)
   @Get()
   async findAllUsers() {
     return this.usersService.findAllUsers();
   }
 
+  @ApiBearerAuth('access_token')
   @UseGuards(JwtGuard)
   @Get('me')
   async getMe(@GetUserDecorator() user: User) {
     return user;
   }
 
+  @ApiBearerAuth('access_token')
   @UseGuards(JwtGuard)
   @Get(':uid')
-  async findUserByUid(@Param('uid') uid: number) {
-    if (typeof uid === 'number') return this.usersService.findUserByUid(uid);
+  async findUserByUid(@Param('uid', ParseIntPipe) uid: number) {
+    return this.usersService.findUserByUid(uid);
   }
 
+  @ApiBearerAuth('access_token')
   @UseGuards(JwtGuard)
   @Put()
   updateUser(@GetUserDecorator() user: User, @Body() dto: UpdateUserDTO) {
     return this.usersService.updateUser(user, dto);
   }
 
+  @ApiBearerAuth('access_token')
   @UseGuards(JwtGuard)
   @Delete()
-  deleteUserById(@Body('uid') uid: number) {
-    if (typeof uid === 'number') return this.usersService.deleteUserById(uid);
+  deleteUserById(@Body('uid', ParseIntPipe) uid: number) {
+    return this.usersService.deleteUserById(uid);
   }
 }
