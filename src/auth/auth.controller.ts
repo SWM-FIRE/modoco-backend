24419f5da22a7c7c43ad90494421ce6010b61426
Juhyeong Ko <dury.ko@gmail.com>
2022-09-09 14:08:52 +0900

    FIRE-566 FIRE-566 Add passport-kakao dependency

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('kakao')
+  loginKakao(): string {
+    return 'works';
+  }
+}
