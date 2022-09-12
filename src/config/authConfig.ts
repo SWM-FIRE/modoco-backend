045c30be78342499af3c8f9d5258141cbb703613
Juhyeong Ko <dury.ko@gmail.com>
2022-09-12 23:45:53 +0900

    FIRE-565 Add github login strategy config

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -3,4 +3,8 @@ export default () => ({
   KAKAO_CALLBACK_URL:
     process.env.KAKAO_CALLBACK_URL ||
     'http://localhost:3333/api/v1/auth/kakao/oauth',
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '',
+  GITHUB_CALLBACK_URL:
+    process.env.GITHUB_CALLBACK_URL ||
+    'http://localhost:3333/api/v1/auth/github/oauth',
 });
