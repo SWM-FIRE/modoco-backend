8b8496158a68d6dd911f6e18a0ff96547e845cd0
Juhyeong Ko <dury.ko@gmail.com>
2022-09-22 16:53:31 +0900

    FIRE-494 Add friend request

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { GetUserDecorator } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { CreateFriendDto } from './dto';
+import { FriendsService } from './friends.service';
 
 @Controller('friends')
-export class FriendsController {}
+@UseGuards(JwtGuard)
+export class FriendsController {
+  constructor(private readonly friendsService: FriendsService) {}
+
+  // friend requests
+  // add friend
+  @Post()
+  addFriend(@Body() dto: CreateFriendDto, @GetUserDecorator() user) {
+    return this.friendsService.addFriend(dto, user);
+  }
+}
