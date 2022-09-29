67f107f3853c34107310c402c84584208de83014
Juhyeong Ko <dury.ko@gmail.com>
2022-09-29 16:02:32 +0900

    Refactor friend controller

---
+++ src/friends/decorators/friend.document.decorators.ts
@@ -0,0 +1,118 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+} from '@nestjs/swagger';
+import { API_DOC_TYPE } from '../constants/docs.enum';
+import { STATUS } from '../constants/status.enum';
+import { TYPES } from '../constants/types.enum';
+
+export function FriendDocumentHelper(docType: API_DOC_TYPE) {
+  switch (docType) {
+    case API_DOC_TYPE.ADD_FRIEND:
+      return addFriendDecorators();
+    case API_DOC_TYPE.GET_FRIENDSHIPS:
+      return getFriendshipDecorators();
+    case API_DOC_TYPE.ACCEPT_FRIENDSHIP:
+      return acceptFriendshipDecorators();
+    case API_DOC_TYPE.DELETE_FRIENDSHIP:
+      return deleteFriendshipDecorators();
+  }
+}
+
+function addFriendDecorators() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '친구 요청',
+      description: '친구 요청을 합니다. PENDING 상태가 됩니다.',
+    }),
+    ApiCreatedResponse({
+      description: '친구 요청 성공',
+    }),
+  );
+}
+
+function getFriendshipDecorators() {
+  return applyDecorators(
+    ApiQuery({
+      name: 'status',
+      enum: STATUS,
+      required: false,
+      description: '친구 관계를 쿼리 파라미터로 필터링해서 가져옵니다.',
+    }),
+    ApiQuery({
+      name: 'type',
+      enum: TYPES,
+      required: false,
+      description:
+        '친구 요청 종류로 필터링합니다. RECEIVED: 나에게 온 요청, SENT: 내가 보낸 요청',
+    }),
+    ApiQuery({
+      name: 'friend',
+      type: Number,
+      required: false,
+      description:
+        '다른 사람과 나의 친구 관계를 조회할 때 사용합니다. 확인하고 싶은 사람의 friend uid를 입력하세요.',
+    }),
+    ApiOperation({
+      summary: '친구 관계 가져오기',
+    }),
+    ApiOkResponse({
+      description: '모든 친구 요청, 신청, 수락 상태를 반환합니다.',
+      schema: {
+        example: [
+          {
+            status: 'PENDING',
+            type: 'RECEIVER',
+            receiver: {
+              uid: 3,
+              nickname: '주형',
+              email: '주형@a.com',
+              avatar: 4,
+            },
+          },
+          {
+            status: 'ACCEPTED',
+            type: 'SENDER',
+            sender: {
+              uid: 2,
+              nickname: '영기',
+              email: '영기@a.com',
+              avatar: 1,
+            },
+          },
+          {
+            status: 'PENDING',
+            type: 'RECEIVER',
+            sender: {
+              uid: 5,
+              nickname: '하령',
+              email: '하령@a.com',
+              avatar: 5,
+            },
+          },
+        ],
+      },
+    }),
+  );
+}
+
+function acceptFriendshipDecorators() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '친구 요청 수락',
+      description: '친구 요청을 수락합니다. ACCEPTED 상태가 됩니다.',
+    }),
+  );
+}
+
+function deleteFriendshipDecorators() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '친구 관계 삭제',
+      description: '친구 관계를 삭제합니다.',
+    }),
+  );
+}
--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -10,19 +10,13 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import {
-  ApiCreatedResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiQuery,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { GetUserDecorator } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 import { ApiAuthDocument } from 'src/common/decorators/swagger/auth.document';
-import { STATUS } from './constants/status.enum';
-import { TYPES } from './constants/types.enum';
+import { API_DOC_TYPE } from './constants/docs.enum';
+import { FriendDocumentHelper } from './decorators/friend.document.decorators';
 import { CreateFriendDto, DeleteFriendDto, UpdateFriendDto } from './dto';
 import { FriendsService } from './friends.service';
 import { FriendshipQueryParams } from './types/friendship.type';
@@ -35,80 +29,15 @@ export class FriendsController {
 
   // friend requests
   // add friend
+  @FriendDocumentHelper(API_DOC_TYPE.ADD_FRIEND)
   @ApiAuthDocument()
-  @ApiOperation({
-    summary: '친구 요청',
-    description: '친구 요청을 합니다. PENDING 상태가 됩니다.',
-  })
-  @ApiCreatedResponse({
-    description: '친구 요청 성공',
-  })
   @Post()
   addFriend(@Body() dto: CreateFriendDto, @GetUserDecorator() user) {
     return this.friendsService.addFriend(user.uid, dto.friend);
   }
 
   // get friendship table
-  @ApiQuery({
-    name: 'status',
-    enum: STATUS,
-    required: false,
-    description: '친구 관계를 쿼리 파라미터로 필터링해서 가져옵니다',
-  })
-  @ApiQuery({
-    name: 'type',
-    enum: TYPES,
-    required: false,
-    description:
-      '친구 요청 종류. RECEIVED: 나에게 온 요청, SENT: 내가 보낸 요청',
-  })
-  @ApiQuery({
-    name: 'friend',
-    type: Number,
-    required: false,
-    description:
-      '다른 사람과 나의 친구 관계를 조회할 때 사용. friend uid를 입력',
-  })
-  @ApiOperation({
-    summary: '친구 관계 가져오기',
-  })
-  @ApiOkResponse({
-    description: '모든 친구 요청, 신청, 수락 상태를 반환',
-    schema: {
-      example: [
-        {
-          status: 'PENDING',
-          type: 'RECEIVER',
-          receiver: {
-            uid: 3,
-            nickname: '주형',
-            email: '주형@a.com',
-            avatar: 4,
-          },
-        },
-        {
-          status: 'ACCEPTED',
-          type: 'SENDER',
-          sender: {
-            uid: 2,
-            nickname: '영기',
-            email: '영기@a.com',
-            avatar: 1,
-          },
-        },
-        {
-          status: 'PENDING',
-          type: 'RECEIVER',
-          sender: {
-            uid: 5,
-            nickname: '하령',
-            email: '하령@a.com',
-            avatar: 5,
-          },
-        },
-      ],
-    },
-  })
+  @FriendDocumentHelper(API_DOC_TYPE.GET_FRIENDSHIPS)
   @ApiAuthDocument()
   @Get()
   getFriendship(
@@ -120,10 +49,7 @@ export class FriendsController {
   }
 
   // accept friend request
-  @ApiOperation({
-    summary: '친구 요청 수락',
-    description: '친구 요청을 수락합니다. ACCEPTED 상태가 됩니다.',
-  })
+  @FriendDocumentHelper(API_DOC_TYPE.ACCEPT_FRIENDSHIP)
   @ApiAuthDocument()
   @Put()
   acceptFriendRequest(@Body() dto: UpdateFriendDto, @GetUserDecorator() user) {
@@ -131,10 +57,7 @@ export class FriendsController {
   }
 
   // delete friendship
-  @ApiOperation({
-    summary: '친구 관계 삭제',
-    description: '친구 관계를 삭제합니다.',
-  })
+  @FriendDocumentHelper(API_DOC_TYPE.DELETE_FRIENDSHIP)
   @ApiAuthDocument()
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete()
